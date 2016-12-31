e910aeb2cb75be5dccfa66e6b8e2a3d6e1300467
chEnru0524 <r10106666@gmail.com>
2016-12-31 18:31:46 +0800

    角色選擇更改(未完)

--- Orientan/src/orientan/ImageSetChooser/ImageSetChooser.java
+++ Orientan/src/orientan/ImageSetChooser/ImageSetChooser.java
@@ -8,26 +8,93 @@ package orientan.ImageSetChooser;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import static javafx.application.Application.launch;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
+
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  *
  * @author ASUS
  */
-public class ImageSetChooser {
+public class ImageSetChooser extends Application {
 
-    private List<File> imageurl=new ArrayList<File>();
+    private List<File> ImageFile = new ArrayList<File>();
 
-    public ImageSetChooser() {
-        int i = 0;
+    @Override
+    public void start(Stage primaryStage) {
         for (File f : new File(System.getProperty("user.dir") + "\\img").listFiles()) {
-            imageurl.add(f);
-            System.out.println(imageurl.get(i));
-            i++;
+            ImageFile.add(f);
+            System.out.println(f);
+            
         }
-        //4、5為圖包資料夾
+          CheckBox[] choose = new CheckBox[2];
+        CheckBox chose_one = new CheckBox(ImageFile.get(3).getName().toString());
+        
+        chose_one.setSelected(false);
+        chose_one.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> ov,
+                    Boolean old_val, Boolean new_val) {
+                System.out.println("chose one chose");
+            }
+
+        });
+        CheckBox chose_two = new CheckBox(ImageFile.get(4).getName().toString());
+        chose_two.setSelected(false);
+        chose_two.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> ov,
+                    Boolean old_val, Boolean new_val) {
+                System.out.println("chose two chose");
+            }
+
+        });
+        choose[0] = chose_one;
+        choose[1] = chose_two;
+        VBox vb=new VBox();
+        vb.getChildren().addAll(choose);
+        Scene sc=new Scene(vb,100,100);
+        primaryStage.setX(200);
+        primaryStage.setY(200);
+        primaryStage.setTitle("chose");
+        primaryStage.setScene(sc);
+        primaryStage.show();
     }
+
     public static void main(String[] args) {
-        ImageSetChooser chooser=new ImageSetChooser();
+        launch(args);
+        System.out.println("chose two chose");
     }
+
 }
+/*public ImageSetChooser() {
+        for (File f : new File(System.getProperty("user.dir") + "\\img").listFiles()) {
+            ImageFile.add(f);
+            System.out.println(f);
+        }
+        CheckBox[] choose = new CheckBox[2];
+        CheckBox chose_one = new CheckBox(ImageFile.get(3).getName().toString());
+        
+        chose_one.setSelected(false);
+        chose_one.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> ov,
+                    Boolean old_val, Boolean new_val) {
+                System.out.println("chose one chose");
+            }
+
+        });
+        CheckBox chose_two = new CheckBox(ImageFile.get(4).getName().toString());
+        chose_two.setSelected(false);
+        chose_two.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> ov,
+                    Boolean old_val, Boolean new_val) {
+                System.out.println("chose two chose");
+            }
+
+        });
+        choose[0] = chose_one;
+        choose[1] = chose_two;
+    }*/

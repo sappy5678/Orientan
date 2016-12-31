3f3860fadbb5abdedbfedab34d41bdb98a5b2d07
chEnru0524 <r10106666@gmail.com>
2016-12-31 20:38:14 +0800

    新增選角功能
    目前無法更改，且須以run file來啟動

--- Orientan/src/orientan/ImageSetChooser/ImageSetChooser.java
+++ Orientan/src/orientan/ImageSetChooser/ImageSetChooser.java
@@ -11,9 +11,16 @@ import java.util.List;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -30,33 +37,56 @@ public class ImageSetChooser extends Application {
         for (File f : new File(System.getProperty("user.dir") + "\\img").listFiles()) {
             ImageFile.add(f);
             System.out.println(f);
-            
+
         }
-          CheckBox[] choose = new CheckBox[2];
-        CheckBox chose_one = new CheckBox(ImageFile.get(3).getName().toString());
-        
+        ToggleGroup group = new ToggleGroup();
+        //RadioButton[] choose = new RadioButton[2];
+        RadioButton chose_one = new RadioButton(ImageFile.get(3).getName().toString());
+        chose_one.setUserData(ImageFile.get(3).getPath());
         chose_one.setSelected(false);
-        chose_one.selectedProperty().addListener(new ChangeListener<Boolean>() {
+        chose_one.setToggleGroup(group);
+        /*chose_one.selectedProperty().addListener(new ChangeListener<Boolean>() {
             public void changed(ObservableValue<? extends Boolean> ov,
                     Boolean old_val, Boolean new_val) {
                 System.out.println("chose one chose");
             }
 
-        });
-        CheckBox chose_two = new CheckBox(ImageFile.get(4).getName().toString());
-        chose_two.setSelected(false);
+        });*/
+        chose_one.setMinSize(10, 20);
+        RadioButton chose_two = new RadioButton(ImageFile.get(4).getName().toString());
+        chose_two.setUserData(ImageFile.get(4).getPath());
+        chose_two.setToggleGroup(group);
+        /*chose_two.setSelected(false);
         chose_two.selectedProperty().addListener(new ChangeListener<Boolean>() {
             public void changed(ObservableValue<? extends Boolean> ov,
                     Boolean old_val, Boolean new_val) {
                 System.out.println("chose two chose");
             }
 
+        });*/
+        chose_two.setMinSize(10, 20);
+        Button set = new Button("Set");
+        set.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+
+                System.out.println("set character");
+
+            }
         });
-        choose[0] = chose_one;
-        choose[1] = chose_two;
-        VBox vb=new VBox();
-        vb.getChildren().addAll(choose);
-        Scene sc=new Scene(vb,100,100);
+        group.selectedToggleProperty().addListener(
+                (ObservableValue<? extends Toggle> ov, Toggle old_toggle,
+                        Toggle new_toggle) -> {
+                    if (group.getSelectedToggle() != null) {
+                        System.out.println(group.getSelectedToggle().getUserData());
+                    }
+                });
+        /*choose[0] = chose_one;
+        choose[1] = chose_two;*/
+        VBox vb = new VBox();
+        vb.getChildren().addAll(chose_one,chose_two);
+        vb.getChildren().add(set);
+        Scene sc = new Scene(vb,100,100);
         primaryStage.setX(200);
         primaryStage.setY(200);
         primaryStage.setTitle("chose");
@@ -66,7 +96,7 @@ public class ImageSetChooser extends Application {
 
     public static void main(String[] args) {
         launch(args);
-        System.out.println("chose two chose");
+        //System.out.println("chose two chose");
     }
 
 }
